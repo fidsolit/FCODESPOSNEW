c2cc7e05f7ed5c2645832bd2411773ba89c3ecf1
Fidelito Pardillo <63651138+fidsolit@users.noreply.github.com>
2024-07-29 01:20:41 +0800

    botchokoy

--- fcodesapp/app/components/editproductForm.tsx
+++ fcodesapp/app/components/editproductForm.tsx
@@ -8,15 +8,48 @@ interface EditProductFormProps {
   id: string;
   brand: string;
   description: string;
+  sellingprice: string;
+  unitprice: string;
+  availableqty: string;
+  sku: string;
+  ram: string;
+  Videocard: string;
+  storage: string;
+  color: string;
+  inches: string;
+  Freebies: string;
+  Warranty: string;
 }
 
 export default function EditProductForm({
   id,
   brand,
   description,
+  sellingprice,
+  unitprice,
+  availableqty,
+  sku,
+  ram,
+  Videocard,
+  storage,
+  color,
+  inches,
+  Freebies,
+  Warranty,
 }: EditProductFormProps) {
   const [newBrand, setNewBrand] = useState<string>(brand);
   const [newDescription, setNewDescription] = useState<string>(description);
+  const [newSellingPrice, setNewSellingPrice] = useState<string>(sellingprice);
+  const [newUnitPrice, setNewUnitPrice] = useState<string>(unitprice);
+  const [newAvailableQty, setNewAvailableQty] = useState<string>(availableqty);
+  const [newSku, setNewSku] = useState<string>(sku);
+  const [newRam, setNewRam] = useState<string>(ram);
+  const [newVideocard, setNewVideocard] = useState<string>(Videocard);
+  const [newStorage, setNewStorage] = useState<string>(storage);
+  const [newColor, setNewColor] = useState<string>(color);
+  const [newInches, setNewInches] = useState<string>(inches);
+  const [newFreebies, setNewFreebies] = useState<string>(Freebies);
+  const [newWarranty, setNewWarranty] = useState<string>(Warranty);
 
   const router = useRouter();
 
@@ -29,7 +62,21 @@ export default function EditProductForm({
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ newBrand, newDescription }),
+        body: JSON.stringify({
+          brand: newBrand,
+          description: newDescription,
+          sellingprice: newSellingPrice,
+          unitprice: newUnitPrice,
+          availableqty: newAvailableQty,
+          sku: newSku,
+          ram: newRam,
+          Videocard: newVideocard,
+          storage: newStorage,
+          color: newColor,
+          inches: newInches,
+          Freebies: newFreebies,
+          Warranty: newWarranty,
+        }),
       });
 
       if (!res.ok) {
@@ -58,6 +105,83 @@ export default function EditProductForm({
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Description"
+      />
+      <input
+        onChange={(e) => setNewSellingPrice(e.target.value)}
+        value={newSellingPrice}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Selling Price"
+      />
+      <input
+        onChange={(e) => setNewUnitPrice(e.target.value)}
+        value={newUnitPrice}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Unit Price"
+      />
+      <input
+        onChange={(e) => setNewAvailableQty(e.target.value)}
+        value={newAvailableQty}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Available Quantity"
+      />
+      <input
+        onChange={(e) => setNewSku(e.target.value)}
+        value={newSku}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="SKU"
+      />
+      <input
+        onChange={(e) => setNewRam(e.target.value)}
+        value={newRam}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="RAM"
+      />
+      <input
+        onChange={(e) => setNewVideocard(e.target.value)}
+        value={newVideocard}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Videocard"
+      />
+      <input
+        onChange={(e) => setNewStorage(e.target.value)}
+        value={newStorage}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Storage"
+      />
+      <input
+        onChange={(e) => setNewColor(e.target.value)}
+        value={newColor}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Color"
+      />
+      <input
+        onChange={(e) => setNewInches(e.target.value)}
+        value={newInches}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Inches"
+      />
+      <input
+        onChange={(e) => setNewFreebies(e.target.value)}
+        value={newFreebies}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Freebies"
+      />
+      <input
+        onChange={(e) => setNewWarranty(e.target.value)}
+        value={newWarranty}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Warranty"
       />
       <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
         Update product
--- fcodesapp/libs/mongodb.js
+++ fcodesapp/libs/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectMongoDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected: this is the mongodb_uri:", process.env.MONGODB_URI);
+    console.log("Connected by :", process.env.MONGODB_URI);
   } catch (error) {
     console.log("cannot connect to mongodb cause =>: ", error);
   }
