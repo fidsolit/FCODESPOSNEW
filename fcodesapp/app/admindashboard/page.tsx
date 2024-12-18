7d08b3fe8338e7f61b5c7504eae0a4afb225e3c1
Fidelito Pardillo <63651138+fidsolit@users.noreply.github.com>
2024-12-18 19:10:08 +0800

    modified admindashboard added security to avoid hackers

--- fcodesapp/app/admindashboard/page.tsx
+++ fcodesapp/app/admindashboard/page.tsx
@@ -1,6 +1,18 @@
+"use client";
 import AllProduct from "../components/allproducts";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../Globalredux/store";
+import { useRouter } from "next/navigation";
 
 export default function AdminDashboard() {
+  const router = useRouter();
+  const authState = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch();
+  if (authState.isAdmin === false) {
+    router.push("/");
+    return null;
+  }
+
   return (
     <>
       <AllProduct />
